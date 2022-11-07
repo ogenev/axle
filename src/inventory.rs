7930b24daf9d55c9afedc44f0506f24d279524c9
Ognyan Genev <ognian.genev@gmail.com>
2022-11-07 13:14:42 +0200

    Load inventory

---
+++ src/inventory.rs
@@ -0,0 +1,52 @@
+use anyhow::anyhow;
+use regex::Regex;
+use std::path::{Path, PathBuf};
+use walkdir::WalkDir;
+
+/// Keeps names of clients and simulators found under root directory
+#[derive(Clone, Debug)]
+pub struct Inventory {
+    base_dir: PathBuf,
+    clients: Vec<PathBuf>,
+    simulators: Vec<PathBuf>,
+}
+
+impl Inventory {
+    pub fn new<P: AsRef<Path>>(base_dir: P) -> Self {
+        Inventory {
+            base_dir: PathBuf::from(base_dir.as_ref()),
+            clients: Vec::new(),
+            simulators: Vec::new(),
+        }
+    }
+
+    pub fn load_inventory(&mut self) {
+        self.clients = find_dockerfiles(self.base_dir.join("clients"));
+        self.simulators = find_dockerfiles(self.base_dir.join("simulators"));
+    }
+
+    /// Returns matching simulator names
+    pub fn match_simulators(&self, expr: &str) -> anyhow::Result<Vec<PathBuf>> {
+        let re = Regex::new(expr).map_err(|err| anyhow!(err))?;
+        let mut matched_simulators = Vec::new();
+        for sim in self.simulators.clone() {
+            if re.is_match(&sim.to_string_lossy()) {
+                matched_simulators.push(sim);
+            }
+        }
+        Ok(matched_simulators)
+    }
+}
+
+fn find_dockerfiles<P: AsRef<Path>>(dir: P) -> Vec<PathBuf> {
+    let mut dockerfiles: Vec<PathBuf> = Vec::new();
+    for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
+        let f_name = entry.file_name().to_string_lossy();
+
+        if f_name.starts_with("Dockerfile") {
+            dockerfiles.push(entry.into_path());
+        }
+    }
+
+    dockerfiles
+}
